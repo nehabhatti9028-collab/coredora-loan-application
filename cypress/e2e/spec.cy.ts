7f2b7fc799e9678f0f9c700024e32f24e88fb786
Neha Bhatti <nehabhatti9028@gmail.com>
2026-08-10 17:10:47 +0530

    Fix Cypress route tests

--- cypress/e2e/spec.cy.ts
+++ cypress/e2e/spec.cy.ts
@@ -1,6 +1,7 @@
+
 describe("LendSwift Loan Application", () => {
   beforeEach(() => {
-    cy.visit("http://localhost:3005");
+    cy.visit("/");
   });
 
   it("loads the homepage", () => {
@@ -9,77 +10,77 @@ describe("LendSwift Loan Application", () => {
   });
 
   it("opens the login page", () => {
-    cy.visit("http://localhost:3005/login");
+    cy.visit("/login");
 
     cy.url().should("include", "/login");
     cy.get("body").should("be.visible");
   });
 
   it("opens the register page", () => {
-    cy.visit("http://localhost:3005/register");
+    cy.visit("/register");
 
     cy.url().should("include", "/register");
     cy.get("body").should("be.visible");
   });
 
   it("redirects unauthenticated users from loan application to login", () => {
-    cy.visit("http://localhost:3005/apply");
+    cy.visit("/apply");
 
     cy.url().should("include", "/login");
     cy.url().should("include", "redirect=%2Fapply");
   });
 
   it("redirects unauthenticated users from calculator to login", () => {
-    cy.visit("http://localhost:3005/calculator");
+    cy.visit("/calculator");
 
     cy.url().should("include", "/login");
     cy.url().should("include", "redirect=%2Fcalculator");
   });
 
   it("opens the loans page", () => {
-    cy.visit("http://localhost:3005/loans");
+    cy.visit("/loans");
 
     cy.url().should("include", "/loans");
     cy.get("body").should("be.visible");
   });
 
   it("redirects unauthenticated users from documents to login", () => {
-    cy.visit("http://localhost:3005/documents");
+    cy.visit("/documents");
 
     cy.url().should("include", "/login");
     cy.url().should("include", "redirect=%2Fdocuments");
   });
 
   it("redirects unauthenticated users from profile to login", () => {
-    cy.visit("http://localhost:3005/profile");
+    cy.visit("/profile");
 
     cy.url().should("include", "/login");
     cy.url().should("include", "redirect=%2Fprofile");
   });
 
   it("redirects unauthenticated users from dashboard to login", () => {
-    cy.visit("http://localhost:3005/dashboard");
+    cy.visit("/dashboard");
 
     cy.url().should("include", "/login");
     cy.url().should("include", "redirect=%2Fdashboard");
   });
 
   it("login page contains form fields", () => {
-    cy.visit("http://localhost:3005/login");
+    cy.visit("/login");
 
     cy.get("input").should("have.length.at.least", 1);
     cy.get("button").should("have.length.at.least", 1);
   });
 
   it("register page contains form fields", () => {
-    cy.visit("http://localhost:3005/register");
+    cy.visit("/register");
 
     cy.get("input").should("have.length.at.least", 1);
     cy.get("button").should("have.length.at.least", 1);
   });
 
   it("calculator route redirects unauthenticated users correctly", () => {
-    cy.visit("http://localhost:3005/calculator");
+    cy.visit("/calculator");
 
     cy.url().should("include", "/login");
     cy.url().should("include", "redirect=%2Fcalculator");
@@ -87,28 +88,28 @@ describe("LendSwift Loan Application", () => {
   });
 
   it("loans page loads successfully", () => {
-    cy.visit("http://localhost:3005/loans");
+    cy.visit("/loans");
 
     cy.get("body").should("be.visible");
     cy.url().should("include", "/loans");
   });
 
   it("documents route redirects unauthenticated users correctly", () => {
-    cy.visit("http://localhost:3005/documents");
+    cy.visit("/documents");
 
     cy.url().should("include", "/login");
     cy.get("body").should("be.visible");
   });
 
   it("home page has visible content", () => {
-    cy.visit("http://localhost:3005");
+    cy.visit("/");
 
     cy.get("body").should("be.visible");
     cy.get("body").invoke("text").should("not.be.empty");
   });
 
   it("application redirects unauthenticated users to login", () => {
-    cy.visit("http://localhost:3005/apply");
+    cy.visit("/apply");
 
     cy.url().should("include", "/login");
     cy.url().should("include", "redirect=%2Fapply");
